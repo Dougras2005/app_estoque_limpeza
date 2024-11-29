47c21a09cb2bf93ce02110177b6b11936d210a80
RyanB-Rezende <ryan.b.rezende@gmail.com>
2024-11-29 19:27:03 -0300

    .

--- lib/data/repositories/telefone_repositories.dart
+++ lib/data/repositories/telefone_repositories.dart
@@ -1,5 +1,5 @@
 import 'package:app_estoque_limpeza/core/database_helper.dart';
-import 'package:app_estoque_limpeza/data/model/TelefoneModel/Telefone_model.dart';
+import 'package:app_estoque_limpeza/data/model/telefone_model.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
 class TelefoneRepository {
