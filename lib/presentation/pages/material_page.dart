160105c6d21aff63f0c29345b5ae787f522b0d1a
Gduarte10 <124733302+Gduarte10@users.noreply.github.com>
2024-12-05 19:39:02 -0300

    Update material_page.dart

--- lib/presentation/pages/material_page.dart
+++ lib/presentation/pages/material_page.dart
@@ -1,218 +1,53 @@
-import 'package:flutter/material.dart';
+import 'dart:ffi';
 
-class ProdutosPage extends StatefulWidget {
-  const ProdutosPage({super.key});
+import 'package:app_estoque_limpeza/core/database_helper.dart';
+import 'package:app_estoque_limpeza/data/model/material_model.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
-  @override
-  ProdutosState createState() => ProdutosState();
-}
-
-class ProdutosState extends State<ProdutosPage> {
-  final _formKey = GlobalKey<FormState>();
-  final TextEditingController _codigoController = TextEditingController();
-  final TextEditingController _nomeController = TextEditingController();
-  final TextEditingController _quantidadeController = TextEditingController();
-  final TextEditingController _localController = TextEditingController();
-  final TextEditingController _fornecedorController = TextEditingController();
-  final TextEditingController _tipoCustomController = TextEditingController();
-  final TextEditingController _dataEntradaController = TextEditingController();
-  final TextEditingController _vencimentoController = TextEditingController();
-  String? _tipo;
-
-  void _cadastrarMaterial() {
-    if (_formKey.currentState?.validate() ?? false) {
-      final tipoFinal = _tipo == "Outro" ? _tipoCustomController.text : _tipo;
-
-      print('Código: ${_codigoController.text}');
-      print('Nome: ${_nomeController.text}');
-      print('Quantidade: ${_quantidadeController.text}');
-      print('Tipo: $tipoFinal');
-      print('Data de Entrada: ${_dataEntradaController.text}');
-      print('Vencimento: ${_vencimentoController.text}');
-      print('Local: ${_localController.text}');
-      print('ID do Fornecedor: ${_fornecedorController.text}');
-
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('Material cadastrado com sucesso!')),
-      );
-
-      _codigoController.clear();
-      _nomeController.clear();
-      _quantidadeController.clear();
-      _localController.clear();
-      _fornecedorController.clear();
-      _tipoCustomController.clear();
-      _dataEntradaController.clear();
-      _vencimentoController.clear();
-      setState(() {
-        _tipo = null;
-      });
-    }
+class MaterialRepository {
+  Future<void> insertMaterial(MaterialModel material) async {
+    final db = await DatabaseHelper.initDb();
+    await db.insert(
+      'Material',
+      material.toMap(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
   }
 
-  @override
-  Widget build(BuildContext context) {
-    final inputDecoration = InputDecoration(
-      filled: true,
-      fillColor: Colors.blue[50],
-      labelStyle: const TextStyle(color: Colors.black),
-      hintStyle: const TextStyle(color: Colors.black54),
-      enabledBorder: OutlineInputBorder(
-        borderSide: const BorderSide(color: Colors.blue),
-        borderRadius: BorderRadius.circular(10),
-      ),
-      focusedBorder: OutlineInputBorder(
-        borderSide: const BorderSide(color: Colors.blue, width: 2),
-        borderRadius: BorderRadius.circular(10),
-      ),
-      errorBorder: OutlineInputBorder(
-        borderSide: const BorderSide(color: Colors.red),
-        borderRadius: BorderRadius.circular(10),
-      ),
-      focusedErrorBorder: OutlineInputBorder(
-        borderSide: const BorderSide(color: Colors.red, width: 2),
-        borderRadius: BorderRadius.circular(10),
-      ),
-    );
+  Future<List<MaterialModel>> getMateriais() async {
+    final db = await DatabaseHelper.initDb();
+    final List<Map<String, Object?>> materialMaps = await db.query('Material');
+    return materialMaps.map((map) {
+      return MaterialModel(
+        idMaterial: map['idMaterial'] as int?,
+        codigo: map['Codigo'] as String,
+        nome: map['Nome'] as String,
+        quantidade: map['Quantidade'] as int,
+        validade: map['Validade'] as String,
+        local: map['Local'] as String,
+        idtipo: map['idtipo'] as int,
+        idfornecedor: map['idfornecedor'] as int,
+        entrada: map['entrada'] as String,
+      );
+    }).toList();
+  }
 
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Cadastro de Produtos'),
-        backgroundColor: Colors.blue,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                TextFormField(
-                  controller: _codigoController,
-                  decoration: inputDecoration.copyWith(labelText: 'Código'),
-                  style: const TextStyle(color: Colors.black),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'O código é obrigatório';
-                    }
-                    return null;
-                  },
-                ),
-                const SizedBox(height: 16),
-                TextFormField(
-                  controller: _nomeController,
-                  decoration: inputDecoration.copyWith(labelText: 'Nome'),
-                  style: const TextStyle(color: Colors.black),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'O nome é obrigatório';
-                    }
-                    return null;
-                  },
-                ),
-                const SizedBox(height: 16),
-                TextFormField(
-                  controller: _quantidadeController,
-                  decoration: inputDecoration.copyWith(labelText: 'Quantidade'),
-                  keyboardType: TextInputType.number,
-                  style: const TextStyle(color: Colors.black),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'A quantidade é obrigatória';
-                    }
-                    if (double.tryParse(value) == null) {
-                      return 'Por favor, insira um número válido';
-                    }
-                    return null;
-                  },
-                ),
-                const SizedBox(height: 16),
-                DropdownButtonFormField<String>(
-                  value: _tipo,
-                  decoration: inputDecoration.copyWith(labelText: 'Tipo'),
-                  items: ['Perecível', 'Não Perecível', 'Outro']
-                      .map((tipo) => DropdownMenuItem(
-                            value: tipo,
-                            child: Text(
-                              tipo,
-                              style: const TextStyle(color: Colors.black),
-                            ),
-                          ))
-                      .toList(),
-                  onChanged: (value) {
-                    setState(() {
-                      _tipo = value;
-                    });
-                  },
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'O tipo é obrigatório';
-                    }
-                    return null;
-                  },
-                ),
-                if (_tipo == "Outro")
-                  TextFormField(
-                    controller: _tipoCustomController,
-                    decoration:
-                        inputDecoration.copyWith(labelText: 'Descreva o Tipo'),
-                    style: const TextStyle(color: Colors.black),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Descreva o tipo';
-                      }
-                      return null;
-                    },
-                  ),
-                const SizedBox(height: 16),
-                TextFormField(
-                  controller: _dataEntradaController,
-                  decoration:
-                      inputDecoration.copyWith(labelText: 'Data de Entrada'),
-                  keyboardType: TextInputType.datetime,
-                  style: const TextStyle(color: Colors.black),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'A data de entrada é obrigatória';
-                    }
-                    return null;
-                  },
-                ),
-                if (_tipo == "Perecível" || _tipo == "Outro")
-                  TextFormField(
-                    controller: _vencimentoController,
-                    decoration:
-                        inputDecoration.copyWith(labelText: 'Vencimento'),
-                    keyboardType: TextInputType.datetime,
-                    style: const TextStyle(color: Colors.black),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'O vencimento é obrigatório';
-                      }
-                      return null;
-                    },
-                  ),
-                const SizedBox(height: 32),
-                Center(
-                  child: ElevatedButton(
-                    onPressed: _cadastrarMaterial,
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.blue,
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 32, vertical: 12),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                    ),
-                    child: const Text('Cadastrar'),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
+  Future<void> updateMaterial(MaterialModel material) async {
+    final db = await DatabaseHelper.initDb();
+    await db.update(
+      'Material',
+      material.toMap(),
+      where: 'idMaterial = ?',
+      whereArgs: [material.idMaterial],
+    );
+  }
+
+  Future<void> deleteMaterial(int id) async {
+    final db = await DatabaseHelper.initDb();
+    await db.delete(
+      'Material',
+      where: 'idMaterial = ?',
+      whereArgs: [id],
     );
   }
 }
