d0a1f126bc5f6870ed78cbd2611d549367c5fb14
Gduarte10 <124733302+Gduarte10@users.noreply.github.com>
2024-12-05 19:33:02 -0300

    Update material_page.dart

--- lib/presentation/pages/material_page.dart
+++ lib/presentation/pages/material_page.dart
@@ -1,5 +1,9 @@
+import 'package:app_estoque_limpeza/data/model/fornecedor_model.dart';
+import 'package:app_estoque_limpeza/data/model/material_model.dart';
+import 'package:app_estoque_limpeza/data/repositories/fornecedor_repository.dart';
+import 'package:app_estoque_limpeza/data/repositories/material_repositories.dart';
+import 'package:app_estoque_limpeza/data/repositories/tipo_repositories.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart'; // Import necessário para DateFormat
 
 class ProdutosPage extends StatefulWidget {
   const ProdutosPage({super.key});
@@ -14,60 +18,96 @@ class ProdutosState extends State<ProdutosPage> {
   final TextEditingController _nomeController = TextEditingController();
   final TextEditingController _quantidadeController = TextEditingController();
   final TextEditingController _localController = TextEditingController();
-  final TextEditingController _fornecedorController = TextEditingController();
+  final TextEditingController _fornecedorCustomController =
+      TextEditingController(); // Novo campo para fornecedor personalizado
   final TextEditingController _tipoCustomController = TextEditingController();
-  DateTime? _vencimento;
-  DateTime? _dataEntrada;
-  String? _tipo;
+  final TextEditingController _dataEntradaController = TextEditingController();
+  final TextEditingController _vencimentoController = TextEditingController();
 
-  Future<void> _selecionarData(BuildContext context, bool isDataEntrada) async {
-    final DateTime? dataSelecionada = await showDatePicker(
-      context: context,
-      initialDate: DateTime.now(),
-      firstDate: DateTime(2000),
-      lastDate: DateTime(2100),
-    );
-    if (dataSelecionada != null) {
+  String? _tipo;
+  String? _fornecedor;
+  List<String> _fornecedores =
+      []; // Lista para armazenar fornecedores carregados
+
+  // Instâncias dos repositórios
+  final MaterialRepository _materialRepository = MaterialRepository();
+  final TipoRepository _tipoRepository = TipoRepository();
+  final FornecedorRepository _fornecedorRepository = FornecedorRepository();
+
+  @override
+  void initState() {
+    super.initState();
+    _listaDeFornecedores(); // Carrega os fornecedores ao inicializar
+  }
+
+  // Função para carregar a lista de fornecedores
+  _listaDeFornecedores() async {
+    try {
+      List<String> fornecedores =
+          await _fornecedorRepository.getNomesFornecedores();
       setState(() {
-        if (isDataEntrada) {
-          _dataEntrada = dataSelecionada;
-        } else {
-          _vencimento = dataSelecionada;
-        }
+        _fornecedores = fornecedores; // Atualiza a lista de fornecedores
       });
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Erro ao carregar fornecedores: $e')),
+      );
     }
   }
 
-  void _cadastrarMaterial() {
+  Future<void> _cadastrarMaterial() async {
     if (_formKey.currentState?.validate() ?? false) {
-      final tipoFinal = _tipo == "Outro" ? _tipoCustomController.text : _tipo;
+      try {
+        // Determina o tipo do material
+        final idTipo = await _tipoRepository.getIdByTipo(_tipo!);
 
-      print('Código: ${_codigoController.text}');
-      print('Nome: ${_nomeController.text}');
-      print('Quantidade: ${_quantidadeController.text}');
-      print('Tipo: $tipoFinal');
-      print(
-          'Data de Entrada: ${_dataEntrada != null ? DateFormat('dd/MM/yyyy').format(_dataEntrada!) : 'Não selecionada'}');
-      print(
-          'Vencimento: ${_vencimento != null ? DateFormat('dd/MM/yyyy').format(_vencimento!) : 'Não selecionado'}');
-      print('Local: ${_localController.text}');
-      print('ID do Fornecedor: ${_fornecedorController.text}');
+        // Determina o fornecedor do material
+        final idFornecedor =
+            await _fornecedorRepository.getIdByForenecedor(_fornecedor!);
 
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('Material cadastrado com sucesso!')),
-      );
+        // Cria o objeto Material
+        final material = MaterialModel(
+          idMaterial: null, // ID gerado automaticamente
+          codigo: _codigoController.text,
+          nome: _nomeController.text,
+          quantidade: int.parse(_quantidadeController.text),
+          validade: _vencimentoController.text.isNotEmpty
+              ? _vencimentoController.text
+              : null, // Pode ser nulo se não aplicável
+          local: _localController.text,
+          idtipo: idTipo!,
+          idfornecedor: idFornecedor!,
+          entrada: _dataEntradaController.text,
+        );
 
-      _codigoController.clear();
-      _nomeController.clear();
-      _quantidadeController.clear();
-      _localController.clear();
-      _fornecedorController.clear();
-      _tipoCustomController.clear();
-      setState(() {
-        _vencimento = null;
-        _dataEntrada = null;
-        _tipo = null;
-      });
+        // Insere o material no banco de dados
+        await _materialRepository.insertMaterial(material);
+
+        // Exibe mensagem de sucesso
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Material cadastrado com sucesso!')),
+        );
+
+        // Limpa os campos do formulário
+        _codigoController.clear();
+        _nomeController.clear();
+        _quantidadeController.clear();
+        _localController.clear();
+        _fornecedorCustomController.clear();
+        _tipoCustomController.clear();
+        _dataEntradaController.clear();
+        _vencimentoController.clear();
+        setState(() {
+          _tipo = null;
+          _fornecedor = null;
+        });
+      } catch (e, stackTrace) {
+        debugPrint(
+            'Erro: $e\n$stackTrace'); // Para ver mais detalhes no console
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Erro ao carregar fornecedores: $e')),
+        );
+      }
     }
   }
 
@@ -149,6 +189,19 @@ class ProdutosState extends State<ProdutosPage> {
                   },
                 ),
                 const SizedBox(height: 16),
+                TextFormField(
+                  controller: _localController,
+                  decoration: inputDecoration.copyWith(labelText: 'Local'),
+                  keyboardType: TextInputType.text,
+                  style: const TextStyle(color: Colors.black),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'O local é obrigatória';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 16),
                 DropdownButtonFormField<String>(
                   value: _tipo,
                   decoration: inputDecoration.copyWith(labelText: 'Tipo'),
@@ -173,7 +226,6 @@ class ProdutosState extends State<ProdutosPage> {
                     return null;
                   },
                 ),
-                const SizedBox(height: 16),
                 if (_tipo == "Outro")
                   TextFormField(
                     controller: _tipoCustomController,
@@ -188,80 +240,75 @@ class ProdutosState extends State<ProdutosPage> {
                     },
                   ),
                 const SizedBox(height: 16),
-                TextFormField(
-                  controller: _fornecedorController,
-                  decoration:
-                      inputDecoration.copyWith(labelText: 'ID do Fornecedor'),
-                  keyboardType: TextInputType.number,
-                  style: const TextStyle(color: Colors.black),
+                // Dropdown para selecionar fornecedor, agora com lista dinâmica
+                DropdownButtonFormField<String>(
+                  value: _fornecedor,
+                  decoration: inputDecoration.copyWith(labelText: 'Fornecedor'),
+                  items: _fornecedores
+                      .map((fornecedor) => DropdownMenuItem(
+                            value: fornecedor, // Ou outro campo identificador
+                            child: Text(
+                              fornecedor, // Exibe o nome do fornecedor
+                              style: const TextStyle(color: Colors.black),
+                            ),
+                          ))
+                      .toList(),
+                  onChanged: (value) {
+                    setState(() {
+                      _fornecedor = value;
+                    });
+                  },
                   validator: (value) {
                     if (value == null || value.isEmpty) {
-                      return 'O ID do fornecedor é obrigatório';
-                    }
-                    if (int.tryParse(value) == null) {
-                      return 'Por favor, insira um número válido';
+                      return 'O fornecedor é obrigatório';
                     }
                     return null;
                   },
                 ),
-                const SizedBox(height: 16),
-                TextFormField(
-                  controller: _localController,
-                  decoration: inputDecoration.copyWith(labelText: 'Local'),
-                  style: const TextStyle(color: Colors.black),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'O local é obrigatório';
-                    }
-                    return null;
-                  },
-                ),
-                const SizedBox(height: 16),
-                Row(
-                  children: [
-                    Text(
-                      _dataEntrada != null
-                          ? 'Data de Entrada: ${DateFormat('dd/MM/yyyy').format(_dataEntrada!)}'
-                          : 'Data de Entrada: Não selecionada',
-                      style: const TextStyle(color: Colors.black),
-                    ),
-                    const Spacer(),
-                    ElevatedButton(
-                      onPressed: () => _selecionarData(context, true),
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.blue,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                      ),
-                      child: const Text('Selecionar Data'),
-                    ),
-                  ],
-                ),
-                if (_tipo == "Perecível" || _tipo == "Outro") ...[
-                  const SizedBox(height: 16),
-                  Row(
-                    children: [
-                      Text(
-                        _vencimento != null
-                            ? 'Vencimento: ${DateFormat('dd/MM/yyyy').format(_vencimento!)}'
-                            : 'Vencimento: Não selecionado',
-                        style: const TextStyle(color: Colors.black),
-                      ),
-                      const Spacer(),
-                      ElevatedButton(
-                        onPressed: () => _selecionarData(context, false),
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.blue,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                        ),
-                        child: const Text('Selecionar Data'),
-                      ),
-                    ],
+
+                // Campo personalizado para fornecedor se a opção "Outro" for selecionada
+                if (_fornecedor == "Outro")
+                  TextFormField(
+                    controller: _fornecedorCustomController,
+                    decoration: inputDecoration.copyWith(
+                        labelText: 'Descreva o Fornecedor'),
+                    style: const TextStyle(color: Colors.black),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Descreva o fornecedor';
+                      }
+                      return null;
+                    },
                   ),
-                ],
+                const SizedBox(height: 16),
+                TextFormField(
+                  controller: _dataEntradaController,
+                  decoration:
+                      inputDecoration.copyWith(labelText: 'Data de Entrada'),
+                  keyboardType: TextInputType.datetime,
+                  style: const TextStyle(color: Colors.black),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'A data de entrada é obrigatória';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 16),
+                if (_tipo == "Perecível" || _tipo == "Outro")
+                  TextFormField(
+                    controller: _vencimentoController,
+                    decoration:
+                        inputDecoration.copyWith(labelText: 'Vencimento'),
+                    keyboardType: TextInputType.datetime,
+                    style: const TextStyle(color: Colors.black),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'O vencimento é obrigatório';
+                      }
+                      return null;
+                    },
+                  ),
                 const SizedBox(height: 32),
                 Center(
                   child: ElevatedButton(
